a29c7f912573f492c7b86d1d6af3c1d140581563
ekiprop <evanskiprop1@gmail.com>
2019-11-14 18:28:58 +0300

    search sidemenu changes

--- resources/views/affiliate/searchproducts.blade.php
+++ resources/views/affiliate/searchproducts.blade.php
@@ -17,10 +17,9 @@
       <section class="section-margin--small mb-5" style=" opacity: 0.9;">
         <div class="container">
           <div class="row">
-                <div class="col-xl-3 col-lg-4 col-md-5">
-
+       
     @include('affiliate.sidemenu')
-                </div>
+                
 
         <div class=" col-xl-9 col-lg-9 col-md-7" id="productsstart">
                 <div class="card">
